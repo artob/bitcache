956eeab3599777ce14fbe2948198afa39646f973
Arto Bendiken <arto@bendiken.net>
2026-08-27 11:17:00 -0700

    Define the initial SQLite schema.

---
+++ rust/lib/bitcache-turso/etc/schema.sql
@@ -0,0 +1,41 @@
+-- This is free and unencumbered software released into the public domain.
+
+DROP TABLE IF EXISTS "bitcache_meta";
+DROP TABLE IF EXISTS "bitcache_data";
+DROP TABLE IF EXISTS "bitcache_blob";
+DROP TABLE IF EXISTS "bitcache_config";
+
+CREATE TABLE IF NOT EXISTS "bitcache_config" (
+    "key" text NOT NULL,
+    "val" numeric NOT NULL,
+    PRIMARY KEY ("key"),
+    CHECK (length("key") > 0)
+);
+
+INSERT INTO "bitcache_config" ("key", "val") VALUES ('schema', 1);
+INSERT INTO "bitcache_config" ("key", "val") VALUES ('epoch', 1767225600); -- 2026-01-01T00:00:00Z
+
+CREATE TABLE IF NOT EXISTS "bitcache_blob" (
+    "id" integer NOT NULL,
+    "blake3" blob NOT NULL,
+    PRIMARY KEY ("id" AUTOINCREMENT),
+    UNIQUE ("blake3"),
+    CHECK (length("blake3") = 32)
+);
+
+CREATE TABLE IF NOT EXISTS "bitcache_data" (
+    "id" integer NOT NULL REFERENCES "bitcache_blob"("id"),
+    "encoding" integer NULL,      -- NULL denotes no encoding
+    "data" blob NOT NULL,
+    PRIMARY KEY ("id")
+); -- WITHOUT ROWID;
+
+CREATE TABLE IF NOT EXISTS "bitcache_meta" (
+    "id" integer NOT NULL REFERENCES "bitcache_blob"("id"),
+    "created" integer NOT NULL,   -- nanoseconds since the epoch
+    "updated" integer NOT NULL,   -- nanoseconds since the epoch
+    "accessed" integer NOT NULL,  -- nanoseconds since the epoch
+    "expires" integer NULL,       -- NULL denotes no expiry
+    "media_type" text NULL,       -- NULL denotes "application/octet-stream"
+    PRIMARY KEY ("id")
+); -- WITHOUT ROWID;
